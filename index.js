50e7e360ec6e4b18bb0aca08dba9715abc6bf544
Tennyson Holloway <tennyson.holloway@gmail.com>
2016-01-07 20:23:02 -0500

    start of form

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express'),
     fs = require('fs'),
-    xml = require('xml');
+    xml = require('xml'),
+    multer = require('multer'),
+    upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/xml', function (req, res) {
 
+});
+
+app.post('/uploadepisode', upload.single('episode-file'), function (req, res) {
+    console.log(req.body, req.file);
+    res.send('ok').end();
+});
+
+app.post('/updatechannel', upload.single('channel-image'), function (req, res) {
+    console.log(req.body);
 });
 
 var server = app.listen(3000);
