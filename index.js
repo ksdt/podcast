e0cfde629053c438f37b5d11729c794f1bdd5195
Tennyson Holloway <tennyson.holloway@gmail.com>
2016-01-08 12:37:34 -0500

    xml renders

--- index.js
+++ index.js
@@ -2,37 +2,130 @@ var express = require('express'),
     fs = require('fs'),
     xml = require('xml'),
     multer = require('multer'),
+    moment = require('moment'),
+    jsonfile = require('jsonfile'),
     podcast = require('podcast'),
-    upload = multer({ dest: 'uploads/', limits: { fileFilter: authorize } });
+    upload = multer({ storage: fileStorage, limits: { fileFilter: authorize } });
 
+
+/* files pass through here to get routed */
+var fileStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './podcast/'+users[req.body.secret]);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.filename);
+    }
+});
+
+/* files pass through here and get rejected/accepted */
 function authorize(req, file, cb) {
-    if (users[req.body.secret]) {
-        cb(null, true);
+    if (users[req.body.secret]) { //password matches a user
+        cb(null, true); //accept
     } else {
-        cb(null, false);
+        cb(null, false); //reject
     }
 }
 
 var app = express();
 
 app.use(express.static('public'));
+app.use(express.static('uploads'));
+app.use('/podcast', express.static('podcast'));
 
-app.get('/xml', function (req, res) {
+app.get('/rss/:user', function (req, res) {
+    /* build feed for user */
+    if (/^[a-z0-9]+$/i.test(req.params.user)) {
+        if (fs.existsSync('./podcast/'+req.params.user+'/')) {
+            if (fs.existsSync('./podcast/'+req.params.user+'/feed.json')) {
+                var feed = jsonfile.readFileSync('./podcast/'+req.params.user+'/feed.json');
+                if (!feed)
+                    res.send('could not find channel information').end();
+            } else {
+                res.send('could not find channel information. please update it').end();
+            }
 
+            var userPodcast = new podcast(feed);
+
+            var items = [];
+            /* read files in items directory */
+            fs.readdir('./podcast/'+req.params.user+'/items', function (err, files) {
+                /* parse json from each file, then add to podcast feed */
+                files.forEach(function (file) {
+                    items.push(userPodcast.item(jsonfile.readFileSync('./podcast/'+req.params.user+'/items/'+file)));
+                });
+
+                res.set('Content-Type', 'application/rss+xml');
+                res.send(userPodcast.xml('    '));
+                res.end();
+            });
+
+        } else {
+            res.send('user not found').end();
+        }
+    } else {
+        res.send('invalid user').end();
+    }
 });
 
 app.post('/uploadepisode', upload.single('episode-file'), function (req, res) {
-    console.log(req.body, req.file);
     /* check if user exists */
     if (users[req.body.secret]) {
-
+        console.log(req.body, req.file);
+        var item = {};
+        item.title = req.body['episode-title'];
+        item.description = req.body['episode-description'];
+        item.itunesSubtitle = req.body['episode-subtitle'];
+        item.itunesSummary = req.body['episode-description'];
+        item.itunesDuration = req.body['episode-duration'];
+        item.date = new Date();
+        item.enclosure = {
+            url: 'ksdt.ucsd.edu/podcast/'+users[req.body.secret]+'/'+req.file.filename,
+            file: req.file.path
+        }
+        jsonfile.writeFile('./podcast/' +
+                users[req.body.secret] +
+                '/items/' +
+                item.title +
+                moment().format('M-D-YYY') +
+                '.json', item, function (err) {
+            console.log(err);
+        });
+        res.send('uploaded. check rss feed to verify').end();
     } else {
-        res.send('secret wrong. if u believe this is an error, please contact thollowa@ucsd.edu').end();
+        res.send('secret wrong. if you believe this is an error, please contact thollowa@ucsd.edu').end();
     }
 });
 
 app.post('/updatechannel', upload.single('channel-image'), function (req, res) {
-    console.log(req.body);
+    if (users[req.body.secret]) {
+        console.log(req.body, req.file);
+        var channel = {};
+        channel.title = req.body['channel-title'];
+        channel.description = req.body['channel-summary'];
+        channel.author = req.body['channel-author'];
+        channel.feed_url = 'https://ksdt.org/podcast/rss/'+users[req.body.secret];
+        channel.site_url = 'https://ksdt.org';
+        channel.image_url = 'https://ksdt.ucsd.edu/podcast/'+users[req.body.secret]+'/'+req.file.filename;
+        channel.webMaster = 'Tennyson Holloway <thollowa@ucsd.edu>';
+        channel.language = 'en-us';
+        channel.categories = [req.body['channel-category']];
+
+        channel.itunesSummary = req.body['channel-summary'];
+        channel.itunesAuthor = req.body['channel-author'];
+        channel.itunesExplicit = req.body['channel-explicit'];
+        channel.itunesCategory = [ { text: req.body['channel-category'] } ];
+        channel.itunesImage = 'https://ksdt.ucsd.edu/podcast/'+users[req.body.secret]+'/'+req.file.filename;
+
+        jsonfile.writeFile('./podcast/' +
+                users[req.body.secret] +
+                '/feed.json', channel, function (err) {
+            console.log(err);
+        });
+        res.send('great success. check rss feed to verify');
+    } else {
+        res.send('secret wrong. if you believe this is an error, please contact thollowa@ucsd.edu').end();
+    }
 });
 
 
@@ -45,8 +138,15 @@ fs.readFile('.pass', function (err, data) {
         console.log(data.toString());
         data.toString().split('\n').forEach(function(line) {
             var user_pass = line.split(' ');
-            if (user_pass[0])
+            if (user_pass[0]) {
                 users[user_pass[1]] = user_pass[0];
+                /* create the user's folder if it doesn't exist*/
+                if (!fs.existsSync('./podcast/'+user_pass[0])) {
+                    fs.mkdirSync('./podcast/'+user_pass[0]);
+                }
+                if (!fs.existsSync('./podcast/'+user_pass[0]+'/items'))
+                    fs.mkdirSync('./podcast/'+user_pass[0]+'/items');
+            }
         });
         console.log(users);
     } else {
